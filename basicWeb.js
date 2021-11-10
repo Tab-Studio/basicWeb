6967cb4f2fa5f1f00c22c223cfc422df90c6a816
鈦匕工作室｜Tab Studio <89435281+Tab-Studio@users.noreply.github.com>
2021-11-10 22:21:23 +0800

    Update basicWeb.js

--- basicWeb.js
+++ basicWeb.js
@@ -29,5 +29,45 @@ if(run){
 	});
 	if(violation > 0){
 		document.body.remove();
+		let body = dc('body'), 
+		mask = dc('div'), 
+		box = dc('div'), 
+		title = dc('h1');
+
+		mask.style.backgroundColor = 'white';
+		mask.style.display = 'flex';
+		mask.style.position = 'fixed';
+		mask.style.width = '100vw';
+		mask.style.height = '100vh';
+		mask.style.top = '0px';
+		mask.style.left = '0px';
+		mask.style.zIndex = '99999999999999999999999999999999';
+		mask.style.flexDirection = 'row';
+    	mask.style.flexWrap = 'nowrap';
+    	mask.style.alignContent = 'center';
+    	mask.style.justifyContent = 'center';
+    	mask.style.alignItems = 'center';
+
+		box.style.backgroundColor = '#00000000';
+		box.style.borderColor = '#000000';
+		box.style.borderStyle = 'solid';
+		box.style.borderWidth = '0.1vw';
+		box.style.width = '80vw';
+		box.style.height = '80vh';
+		box.style.borderRadius = '2vw';
+		box.style.padding = '2vw';
+
+		title.innerText = '遭到防火牆攔截！';
+		title.style.fontSize = '3vw';
+		title.style.color = 'black';
+
+		box.appendChild(title);
+		box.appendChild(dc('hr'));
+		mask.appendChild(box);
+		body.appendChild(mask);
+		document.querySelector('html').appendChild(body);
 	}
+}
+function dc(...e){
+	return(document.createElement(...e));
 }
